fd47b5904d2bd51feb9e37851335814ee918b87b
eenurike <eenurike@gmail.com>
2020-07-30 18:05:12 +0600

    делегирование событий

--- learning-js/js/script.js
+++ learning-js/js/script.js
@@ -1,16 +1,57 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-    let box = document.querySelector(".box");
-    console.log(box);
+let btns = document.querySelectorAll("button"),
+    wrapper = document.querySelector(".btn-block");
 
-    // box.addEventListener("touchstart", (event) => {
-    //     event.preventDefault();
-    //     console.log("start");
-    //     console.log(event.touches);
-    // });
+    // wrapper.addEventListener("click", (event ) => {
+    //     if(event.target && event.target.tagName == "BUTTON") {
+    //         event.target.classList.toggle("red");
+    //     }
+    //     let addBtn = document.createElement("button");
+    //     addBtn.classList.add("red");
+    // wrapper.append(addBtn);
+    //     // if(event.target && event.target.classList.contains("blue")){
+    //     //     event.target.classList.toggle("red");
+    //     // }
+    //     // console.dir(event.target);
 
-    box.addEventListener("touchmove", (event) => {
-        console.log(event.targetTouches[0].pageX);  //[0] это один ралец, типа вилим координаты перемещения пальлца. и можно сделать  чтоб с какогото координата например перелистывался слайд
+    // }) ;
+    
+    btns.forEach((item) => {
+        item.addEventListener("click", () => {
+            console.log("work");      
+        });
     });
-});
+
+    let btn = document.createElement("button");
+    btn.classList.add("red");
+    wrapper.append(btn);
+
+    
+    
+
+// console.log(btns[0].classList.length);
+
+// // console.log(btns[0].classList.item(1));
+
+// btns[1].classList.add("red", "asfjefe");
+// // btns[0].classList.remove("blue");
+// // btns[0].classList.toggle("blue");
+
+// if (btns[1].classList.contains("red")) {
+//     console.log("red");
+// }
+
+// btns[1].classList.add("red");
+
+// btns[0].addEventListener("click", () => {
+//     if (btns[1].classList.contains("red")) {
+//         btns[1].classList.remove("red");
+//     } else {
+//         btns[1].classList.add("red");
+//     }
+// });
+
+// btns[0].addEventListener("click", () => {
+//     btns[1].classList.toggle("red");
+// });
