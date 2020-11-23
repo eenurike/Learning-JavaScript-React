99123de3e98009264fb35f8d521cbb4d28fe9817
eenurike <eenurike@gmail.com>
2020-11-23 09:43:02 +0600

    learningReact

---
+++ testReactApp/my-app/src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../header/header'
+
+const App = () => {
+    return (
+        <div>
+            <h2>Hello!</h2>
+            <Header/>
+        </div>
+        
+    )
+}
+
+export default App;
+
