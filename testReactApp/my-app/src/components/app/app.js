ad6a07671649f82445c6a692f299effb622c73b8
eenurike <eenurike@gmail.com>
2020-11-27 17:24:56 +0600

    state

--- testReactApp/my-app/src/components/app/app.js
+++ testReactApp/my-app/src/components/app/app.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
 import Header from '../header/header'
+import SearchPanel from '../search-panel/search-panel'
+import PostStatusFilter from '../post-status-filter/post-status-filter'
+import PostList from '../post-list/post-list'
+import PostAddForm from '../post-add-form/post-add-form'
+
+import './app.css'
 
 const App = () => {
     return (
         <div>
-            <h2>Hello!</h2>
             <Header/>
+
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList/>
+            <PostAddForm/>
         </div>
         
     )
--- testReactApp/my-app/src/index.js
+++ testReactApp/my-app/src/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/app';
+// import App from './components/app/app';
 // import reportWebVitals from './reportWebVitals';
 
 
+class WhoAmI extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         years: 26
+    //     }
+    //     this.nextYear = this.nextYear.bind(this)  способ 1
+    //     this.nextYear = () => {                      способ 2
+    //         this.setState(state => ({
+    //             years: ++state.years
+    //         }))
+    //     }
+    // }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+    // nextYear() {
+    //     this.setState(state => ({
+    //         years: ++state.years
+    //     }))
+    // }
+
+    state = {
+        years: 26
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+            years: ++state.years
+        }))
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        const years = this.state.years;
+        return (
+                 <>
+                    <button onClick={this.nextYear}>++</button>
+                    <h1>My name is {name}, surname {surname}, years - {years}</h1>
+                    <a href={link}>My profile</a>
+                 </>
+                )
+    }
+}
+
+const All = () => {
+    return(
+        <>
+            <WhoAmI name="Nur" surname="Yelemesov" link="vk.com"/>
+            <WhoAmI name="Liuba" surname="Stolyarova" link="vk.com"/>
+            <WhoAmI name="Temur" surname="Sarsenbaev" link="vk.com"/>
+        </>
+    )
+}
+
+ReactDOM.render(<All/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
