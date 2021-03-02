3b7c39510e549e00229e72095170654d526024cc
eenurike <eenurike@gmail.com>
2021-03-02 21:53:05 +0600

    Практикую practice

--- learning-js/Просто изучение материала/js/practice2.js
+++ learning-js/Просто изучение материала/js/practice2.js
@@ -35,15 +35,41 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
     }
 
-    // if (a === null || a === "" || a.length < 20) {
-    //     personalMovieDB.movies[a] = b;
-    //     console.log('done');
+
+// СПОСОБ 2 с == и с ИЛИ(||),с И(&&) не получается потомучто вседга будет fasle
+    // if (a == null || a == "" || a.length > 20) {
+    //     i--;
+    //     console.log("error");
     // } else {
-    //    i--;
-    //    console.log('error');
+    //     personalMovieDB.movies[a] = b;
+    //     console.log("done");
     // }
-       
 }
+
+// МОЙ СПОСОБ С WHILE
+
+// let a;
+// let b;
+
+// function question1() {
+//    a = prompt("Один из последних просмотренных фильмов?");
+// }
+
+// function question2() {
+//     b = +prompt("rate them");
+// }
+
+// while(a == null || a == "" || a.length > 10 ) {
+//     question1();
+   
+// }
+
+// while(b == null || b == "" || b !== Number(b)) {
+//     question2();
+// }
+
+// personalMovieDB.movies[a] = b;
 
 
 console.log(personalMovieDB);
+
