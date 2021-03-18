eff75d536722d742793767267990208b0d244584
eenurike <eenurike@gmail.com>
2021-03-18 17:50:31 +0600

    Массивы

--- learning-js/Просто изучение материала/js/test.js
+++ learning-js/Просто изучение материала/js/test.js
@@ -10,15 +10,21 @@ const obj = {
     }
 };
 
-// let {name1, width, height, style} = obj;
-// console.log(height);
 
 for (let key in obj) {
-    if (typeof(obj[key]) === "object") {
+    if (typeof(obj[key]) === 'object') {
         for (let i in obj[key]) {
             console.log(`Свойство ${i} и его значение ${obj[key][i]}`);
+           
         }
     } else {
         console.log(`Свойство ${key} и его значение ${obj[key]}`);
+       
+        
     }
 }
+ console.log(Object.keys(obj).length);
+
+
+
+
