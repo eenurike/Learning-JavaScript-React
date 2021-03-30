c27d51aac7f6fbb7dab1a632a836a1eb0db5e170
eenurike <eenurike@gmail.com>
2021-03-30 17:40:58 +0600

    Динамическая типизация

--- learning-js/Просто изучение материала/js/practice4.js
+++ learning-js/Просто изучение материала/js/practice4.js
@@ -14,24 +14,30 @@
 
 
 const personalMovieDB = {
-    count: 0,
+    count: 0,       // Изначальное кол-во фильмов 0
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-    start: function() {
+    start: function () {
         personalMovieDB.count = +prompt("Сколько фильмов вы уже смотрели?");
+        /*
+            Нужда в переменной numberOfFilms отпадает,
+            так как функцию start мы превратили в метод.
+            Когда изучим контекст вызова this,сможем 
+            напрямую обращаться к count
+        */
 
-        while(personalMovieDB.count == "" || personalMovieDB.count == null || isNaN         (personalMovieDB.count)) {
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
             personalMovieDB.count = +prompt("Сколько фильмов вы уже смотрели?");
         }
     },
-    rememberMyFilms: function() {
+    rememberMyFilms: function () {
         for (let i = 0; i < 2; i++) {
             let a = prompt("Один из последних просмотренных фильмов?"),
                 b = prompt('На сколько оцените его?');
-        
-              
+
+
             if (a != null && b != null && a != '' && b != '' && a.length < 20) {
                 personalMovieDB.movies[a] = b;
                 console.log('done');
@@ -41,7 +47,7 @@ const personalMovieDB = {
             }
         }
     },
-    detectPersonalLevel: function() {
+    detectPersonalLevel: function () {
         if (personalMovieDB.count <= 10) {
             console.log("Вы посмотрели довольно мало фильмов");
         } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
@@ -52,28 +58,72 @@ const personalMovieDB = {
             console.log("Произошла ошибка");
         }
     },
-    
-    showMyDB: function(hidden) {
-        if (!hidden) {                
-            
-            console.log(personalMovieDB); 
-        }  else {
+
+    showMyDB: function (hidden) {
+        if (!hidden) {
+
+            console.log(personalMovieDB);
+        } else {
             console.log("Аккаунт приватен!");
-        }  
+        }
     },
-    toggleVisibleMyDB: function() {
-        if(personalMovieDB.privat) {
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
             personalMovieDB.privat = false;
         } else {
             personalMovieDB.privat = true;
         }
+        /*
+            Класический шоблон переключателя, если наш privat 
+            в false, то наш шаблон идет по else пути, и
+            переключает его на true. Если в позиции true,
+            то меняет его на false
+        */
     },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 3; i++) {
-            let question = prompt(`Ваш любимый жанр под номером ${i}?`);
-            personalMovieDB.genres[i-1] = question; 
+    writeYourGenres: function () {
+        // for (let i = 1; i <= 3; i++) {
+
+        //     let question = prompt(`Ваш любимый жанр под номером ${i}?`);
+
+        //     if (question == null || question == "") {
+        //         i--;
+        //         console.log('error');
+        //     } else {
+        //         personalMovieDB.genres[i-1] = question; 
+        //     } 
+        // }
+
+
+        // Альтернативный способ с помощью split
+        for (let i = 0; i < 1; i++) {  
+            /*
+                Так как у нас один вопрос, делаем всего одну итерацию
+                если ввел не правильно, то возвращаем вопрос назад
+            */
+            let question = prompt("Введите любимые жанры через запятую").toLowerCase();
+            /*
+                Так тоже можно запимсывать, что введет пользователь
+                JS сразу преобразует в нижний регистр
+            */
+            if (question == null || question == "") {
+                i--;
+                console.log("error");
+            } else {
+                personalMovieDB.genres = question.split(", ");
+                personalMovieDB.genres.sort();
+                /*
+                    Если пользователь введет какую то букву в 
+                    верхнем регистре, то наша сортировка собьется,
+                    так как по классификации unicode, верхний регистр 
+                    при сортировки выводится первый, поэтому 
+                    в начале указали метод toLowerCase()
+                */
+            }
         }
-        console.log(personalMovieDB);
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i+1} - ${item}`);
+        });
     }
 
 };
@@ -86,13 +136,7 @@ const personalMovieDB = {
 // personalMovieDB.detectPersonalLevel();
 
 
-personalMovieDB.showMyDB(personalMovieDB.privat);
-personalMovieDB.toggleVisibleMyDB();
+// personalMovieDB.showMyDB(personalMovieDB.privat);
+// personalMovieDB.toggleVisibleMyDB();
 
-// personalMovieDB.writeYourGenres();
-
-
-
-
-
-
+personalMovieDB.writeYourGenres();
