c230503d4cf2f93e569a1c11f79e378352af1181
eenurike <eenurike@gmail.com>
2021-04-02 15:55:52 +0600

    Действие с элементами

---
+++ learning-js/Просто изучение материала/js/script16.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let box = document.getElementById('box'),
+    btns = document.getElementByTagName('button'),
+    circles = document.getElementByClassName('circle'),
+    hearts = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart');
+
+console.dir(box);
+/*
+В консоль вывелся обьект box, смотрим его свойства и методы
+
+*/
