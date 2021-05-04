f2e1b73c2ed59f85ad60f1e2631358f6576f8e1e
eenurike <eenurike@gmail.com>
2021-05-04 18:14:28 +0600

    Работа с датами 1

--- learning-js/Практика с заданием Movie/js/practice.js
+++ learning-js/Практика с заданием Movie/js/practice.js
@@ -1,40 +1,78 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    let myObj = {
-        fruits: [
-            "Apple",
-            "Orange",
-            "Kivi",
-            "Banana",
-            "potato"
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против...",
+            "Аппалон 13"
         ]
     };
 
-    let fruitList = document.querySelector('ul'),
-        formAdd = document.querySelector('form'),
-        inputAdd = document.querySelector('input'),
-        buttonAdd = document.querySelector('button');
 
-    fruitList.innerHTML = "";
+    let adv = document.querySelectorAll('.promo__adv > img'),
+        genre = document.querySelector('.promo__genre'),
+        bgImage = document.querySelector('.promo__bg'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        formAdd = document.querySelector('form.add'),
+        inputValue = document.querySelector('.adding__input'),
+        checkbox = document.querySelector('input[type="checkbox"]');
 
-    let updateFruitList = () => {
-        myObj.fruits.forEach((item,i) => {
-            fruitList.innerHTML += `
-                <li>${i+1}: ${item}</li>
-            `;
-        });
-    
-    };
-    updateFruitList();
+    adv.forEach(item => {
+        item.remove();
+    });
+    genre.textContent = "Драма";
+    bgImage.style.backgroundImage = "url('img/bg.jpg')";
+
+
     formAdd.addEventListener('submit', (event) => {
         event.preventDefault();
-        fruitList.innerHTML = "";
-        let newFruits = inputAdd.value;
-        myObj.fruits.push(newFruits);
-        updateFruitList();
-        
+        let newFilm = inputValue.value;
+        let favorite = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 5) {
+                newFilm = `${newFilm.slice(0,5)}...`;
+            }
+
+            if (favorite) {
+                console.log('Добавляем любимый фильм');
+            }
+
+            movieDB.movies.push(newFilm);
+            createMovieList();
+        }
+
+        event.target.reset();
     });
 
+    let createMovieList = () => {
+        movieList.innerHTML = "";
+        movieDB.movies.sort();
+
+        movieDB.movies.forEach((item, i) => {
+            movieList.innerHTML += `
+            <li class="promo__interactive-item">${i+1}: ${item}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((item,i) => {
+            item.addEventListener('click', () => {
+                item.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList();
+            });
+        });
+    };
+
     
+
+
+
+    createMovieList();
 });
