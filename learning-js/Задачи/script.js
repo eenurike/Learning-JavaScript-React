8565a0c2088be791965b21939a7ac65870907c5c
Nurike <eenurike@gmail.com>
2022-09-30 16:04:53 +0600

    Перестановка папок и файлов

---
+++ learning-js/Задачи/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const userName = 'John';
+let userNumber = 25;
+userNumber = 24;
+
+console.log(userNumber);
+
+const storeName = 'bmxShop';
+const storeDescription = {
+  budget: 1000,
+  employees: [
+    'Stat',
+    'Temur',
+    'Madi'
+  ],
+  products: {
+    frame: 330,
+    fork: 130,
+    open: true
+  }
+};
