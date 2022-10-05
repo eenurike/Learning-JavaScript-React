f09b2e5189e33814f92e3c31b5c4f24f0007ac42
Nurike <eenurike@gmail.com>
2022-10-05 17:31:05 +0600

    Фунции(Задачи)

--- learning-js/Задачи/script.js
+++ learning-js/Задачи/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
+function sayHello(name) {
+  return `Привет, ${name}!`;
+}
 
+console.log(sayHello('Нурике'));
 
+function returnNeighboringNumbers(num) {
+  return [5, num, 7];
+}
+
+console.log(returnNeighboringNumbers(6));
